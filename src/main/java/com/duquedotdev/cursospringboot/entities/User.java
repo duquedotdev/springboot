5018cece46291c1c75872e779717570ac839a4e0
Felipe Duque <felipe@duque.dev>
2021-01-19 07:41:42 -0300

    chore: create order and relationship between user and orders

--- src/main/java/com/duquedotdev/cursospringboot/entities/User.java
+++ src/main/java/com/duquedotdev/cursospringboot/entities/User.java
@@ -1,13 +1,14 @@
 package com.duquedotdev.cursospringboot.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 
 @Entity
+@Table(name="tb_user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -19,6 +20,9 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
 	}
@@ -72,7 +76,11 @@ public class User implements Serializable{
         this.password = password;
     }
 
-	@Override
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    @Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
